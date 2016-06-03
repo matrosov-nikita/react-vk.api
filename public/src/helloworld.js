4aec80eb6458e587dc97a9a017db148eae169496
madgrub <matrosov.nikita94@gmail.com>
2016-06-03 19:50:06 +0300

    fix bug with leave context about console.err

--- public/src/helloworld.js
+++ public/src/helloworld.js
@@ -1,84 +1,85 @@
 var MovieRow = React.createClass({
-	render: function() {
-		alert("start render");
-		return (
-			<tr>
-				<td> {this.props.movie.name} </td>
-				<td> {this.props.movie.description} </td>
-			</tr>
-			)
-	}
+    render: function() {
+        alert("start render");
+        return (
+            <tr>
+                <td> {this.props.movie.name} </td>
+                <td> {this.props.movie.description} </td>
+            </tr>
+            )
+    }
 });
 
 var MovieTable = React.createClass({
-	getInitialState: function() {
-		return {movies: [] }
-	},
+    getInitialState: function() {
+        return {movies: [] }
+    },
 
-	getWallPosts: function(groupName) {	  
+    getWallPosts: function(groupName) {   
 
-	       		var count = 100;
-				var self = this;
-   				getCountPostsFromWall(groupName)
+                var count = 100;
+                var self = this;
+                getCountPostsFromWall(groupName)
 
-   				.then((countPosts) => {
-					return countPosts[0];
+                .then((countPosts) => {
+                    alert("dsadasd");
+                    return countPosts[0];
 
-   				})
+                })
 
-   				.then((res)=> {
-   					if (res < 100) {
-   						return getPostsFromWall(groupName, count, 0);
-   					}
+                .then((res)=> {
+                    if (res < 100) {
+                        return getPostsFromWall(groupName, count, 0);
+                    }
 
-   					var arr = Array.apply(null, Array(Math.ceil(res/count)));
-   					
+                    var arr = Array.apply(null, Array(Math.ceil(res/count)));
+                    
 
-   					return arr.reduce(function(chain, element, index) {
-   						return chain.then(function(result) {
+                    return arr.reduce(function(chain, element, index) {
+                        return chain.then(function(result) {
 
-   								sortMoviesByLikes(result);
+                                sortMoviesByLikes(result);
 
-   								  if (self.state.movies.length === 0) {		                     
-			                        	self.setState({movies: result.slice(0,20)});
-	                    			} else {
-			                        self.setState({movies: chooseTop10(self.state.movies, result)});
-		                    	  }
+                                  if (self.state.movies.length === 0) {                          
+                                        self.setState({movies: result.slice(0,20)});
+                                    } else {
+                                    self.setState({movies: chooseTop10(self.state.movies, result)});
+                                  }
 
-   							   return getPostsFromWallWithPause(groupName,count,index*count + count);
-   						})
-   					}, getPostsFromWall(groupName, count, 0));
-   				})
+                               return getPostsFromWallWithPause(groupName,count,index*count + count);
+                        })
+                    }, getPostsFromWall(groupName, count, 0));
+                })
 
-   				.then((result)=> {
-   					sortMoviesByLikes(result);
-   				     self.setState({movies: result.slice(1)});
-   				})
-   				.catch(console.error.bind(window));	   
-	    	 },
+                .then((result)=> {
+                    sortMoviesByLikes(result);
+                     self.setState({movies: result.slice(1)});
+                })
+                .catch(console.error.bind(console));       
+             },
 
-	componentDidMount: function() {
-		this.getWallPosts('nightmares');
-	},
+    componentDidMount: function() {
+        this.getWallPosts('nightmares');
+    },
 
-	render: function() {
-		var movieRows = this.state.movies.map((movie)=> {
-			return (
-				<MovieRow movie={movie} key={movie.id} />
-			)
-		})
-		return (
-			<table className="table table-striped">
-				<thead>
-					<th>Название</th>
-					<th>Описание</th>
-				</thead>
-				<tbody>
-					{movieRows}
-				</tbody>
-			</table>
-		)
-	}
+    render: function() {
+        var movieRows = this.state.movies.map((movie)=> {
+            return (
+                <MovieRow movie={movie} key={movie.id} />
+            )
+        })
+        return (
+            <table className="table table-striped">
+                <thead>
+                    <th>Название</th>
+                    <th>Описание</th>
+                </thead>
+                <tbody>
+                    {movieRows}
+                </tbody>
+            </table>
+        )
+    }
 })
 
 ReactDOM.render(<MovieTable movieURL ='/config.json' />, document.getElementById('box'));
